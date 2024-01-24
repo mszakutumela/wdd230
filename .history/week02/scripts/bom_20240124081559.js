2e75e7bb99d6140de617c0b51d5c7e0d75e106e9
mszakutumela <misszandlovu@gmail.com>
2024-01-24 08:54:23 +0200

    JavaScript: DOM Manipulation is complete

---
+++ .history/week02/scripts/bom_20240124081559.js
@@ -0,0 +1,10 @@
+
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function()
+{
+    
+
+});
---
+++ .history/week02/scripts/bom_20240124083517.js
@@ -0,0 +1,13 @@
+
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function()
+{
+    if (input.value != " ") {
+        const li = document.createElement("li");
+        const deleteButton = document
+    }
+
+});
