90bb0c62e9104021270862b51014d6385bbcd236
mszakutumela <misszandlovu@gmail.com>
2024-01-28 05:28:26 +0200

    W03: Team Activity is complete

---
+++ .history/scripts/getDates_20240128045435.js
@@ -0,0 +1,70 @@
+// get current year
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+// get last modified date
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+
+// Responsive Hamburger Menu
+const hamburgerElement = document.querySelector("#menuButton");
+const navElement = document.querySelectorAll(".menuLinks");
+
+hamburgerElement.addEventListener("click", () => {
+//     navElement.classList.toggle("open"); (Note when I used this code: Uncaught TypeError: Cannot read properties of undefined (reading 'toggle')
+//     at HTMLButtonElement.<anonymous> (getDates.js:14:26)
+// (anonymous) @ getDates.js:14)
+    navElement.forEach(x => x.classList.toggle("open"));
+    // hamburgerElement.forEach(x => x.classList.toggle("open"));
+    hamburgerElement.classList.toggle("open");
+});
+
+// W02: Dark Mode Button - Code pen code method (https://codepen.io/blazzard-jason/pen/ZERawEQ)
+const darkMode = document.querySelector("#darkButton");
+const mainElement = document.querySelector("main");
+
+darkMode.addEventListener("click", () => {
+	if (darkMode.textContent.includes("🕶️")) {
+		mainElement.style.background = "#000";
+		mainElement.style.color = "#fff";
+		darkMode.textContent = "🔆";
+	} else {
+		mainElement.style.background = "#eee";
+		mainElement.style.color = "#000";
+		darkMode.textContent = "🕶️";
+	}
+});
+
+// W03: Visit Counter
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-Is")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+}
+else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-Is", numVisits);
+
+// W03: Maps JavaScript API
+// let map;
+
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// initMap();
+
+
+
+
