4ac180b85728d452a6d61a2c21ef3bb174d3b095
mszakutumela <misszandlovu@gmail.com>
2024-02-19 22:09:11 +0200

    chamber Website in progress

---
+++ .history/chamber/scripts/getDates_20240219213946.js
@@ -0,0 +1,8 @@
+// get current year
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+// get last modified date
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+
