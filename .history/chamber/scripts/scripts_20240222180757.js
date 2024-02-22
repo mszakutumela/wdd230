ad386cd00ceedb1992ec04f72209f1db65425c1d
mszakutumela <misszandlovu@gmail.com>
2024-02-22 23:58:34 +0200

    Chamber join page is complete

---
+++ .history/chamber/scripts/scripts_20240222180757.js
@@ -0,0 +1,47 @@
+// get current year
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+// get last modified date
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+
+// Responsive Hamburger Menu
+const hamburgerElement = document.querySelector("#menuButton");
+const navElement = document.querySelectorAll(".menuLinks");
+
+hamburgerElement.addEventListener("click", () => {
+//     navElement.classList.toggle("open"); (Note when I used this code: Uncaught TypeError: Cannot read properties of undefined (reading 'toggle')
+//     at HTMLButtonElement.<anonymous> (getDates.js:14:26)
+// (anonymous) @ getDates.js:14)
+    navElement.forEach(x => x.classList.toggle("open"));
+    // hamburgerElement.forEach(x => x.classList.toggle("open"));
+    hamburgerElement.classList.toggle("open");
+});
+
+// W02: Dark Mode Button - Code pen code method (https://codepen.io/blazzard-jason/pen/ZERawEQ)
+const darkMode = document.querySelector("#darkButton");
+const mainElement = document.querySelector("main");
+
+darkMode.addEventListener("click", () => {
+	if (darkMode.textContent.includes("🕶️")) {
+		mainElement.style.background = "#000";
+		mainElement.style.color = "#fff";
+		darkMode.textContent = "🔆";
+	} else {
+		mainElement.style.background = "#eee";
+		mainElement.style.color = "#000";
+		darkMode.textContent = "🕶️";
+	}
+});
+
+// Form Submission Date
+// let dateTimeToday = new Date();
+// document.querySelector("input[name='date']").value = dateTimeToday;
+window.onload = function () {
+	document.getElementById("timestamp").value = timeStamp();
+};
+
+function timeStamp(mod) {
+	mod = formatDate
+}
